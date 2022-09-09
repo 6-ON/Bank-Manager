c5b8075400af1d733a0c791f25022097d8bfb99e
=6-ON <=aminhatim6@gmail.com>
2022-09-09 13:00:13 -0400

    update menu logic

--- libs/menu.c
+++ libs/menu.c
@@ -1,6 +1,3 @@
-#define MODE_KEY "stty raw"
-#define MODE_LINE "stty cooked"
-
 // menu choices
 #define MENU_CHOICE_1 1
 #define MENU_CHOICE_2 2
@@ -10,6 +7,7 @@
 #define MENU_CHOICE_6 6
 #define MENU_CHOICE_7 7
 #define MENU_CHOICE_8 8
+#define BACK_TO_MENU -1
 
 // prompts
 #define AMMOUNT_PROMPT "Enter the ammount: "
@@ -20,7 +18,10 @@
 #define MSG_OPERATION_DONE "Operation done successfully"
 // errors messages
 #define ERR_WRONG_INPUT "Wrong input !!"
+#define ERR_OOR "OUT OF RANGE !!"
 #define ERR_ACCOUNT_NOT_FOUND "The account is not found do you want to create an account with the given account number? y/n"
+#define ERR_NEGATIVE_AMMOUT "error: you cant enter a negative ammount"
+#define NOT_ENOUGH_MONEY "YOU DONT HAVE ENGOUGH MONEY FOR THIS OPERATION !!"
 // err nums
 #define WRONG_INPUT 0
 
@@ -28,103 +29,117 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 // my libs includes
 #include "bank_management.h"
 #include "menu.h"
 AccountsList accs;
-
-int cleanGetInt(int *val)
+// get int
+void getInt(int *val)
 {
-    char buff[255];
-
-    fgets(buff, sizeof(buff), stdin);
-    if (sscanf(buff, "%d", val) == WRONG_INPUT)
+    while (true)
     {
-        puts(ERR_WRONG_INPUT);
-        return -1;
+        char buff[255];
+        fgets(buff, sizeof(buff), stdin);
+        if (sscanf(buff, "%d", &val) == WRONG_INPUT)
+        {
+            puts(ERR_WRONG_INPUT);
+            printf(">> ");
+            continue;
+        }
+        break;
     }
-    return 0;
 }
-int cleanGetDouble(double *val)
-{
-    char buff[255];
 
-    fgets(buff, sizeof(buff), stdin);
-    if (sscanf(buff, "%lf", val) == WRONG_INPUT)
+void getDouble(double *val)
+{
+    while (true)
     {
-        puts(ERR_WRONG_INPUT);
-        return -1;
+        char buff[255];
+        fgets(buff, sizeof(buff), stdin);
+        if (sscanf(buff, "%lf", &val) == WRONG_INPUT)
+        {
+            puts(ERR_WRONG_INPUT);
+            printf(">> ");
+            continue;
+        }
+        break;
     }
-    return 0;
 }
 // returns  index of  the account selected
 int selectFromTable()
 {
-    // system(CLS);
-    printAccountsTable(accs);
-    puts("<-- 0 ");
+    printAccountsTable(accs); // print Table
+    puts("<-- 0 ");           // add 0
+prompt:
     int index;
-    while (true)
+    printf(">> ");
+    getInt(&index); // prompt index and set it
+
+    if (index == 0)
     {
-        printf(">> ");
-        if (cleanGetInt(&index) == -1)
-            continue;
-        if (index > 0 && index < accs.length + 1)
-            break;
-        else if (index == 0)
-        {
-            puts("going back to menu...");
-            return -1;
-        }
-        else
-            puts(ERR_WRONG_INPUT);
+        puts("going back to menu...");
+        return BACK_TO_MENU;
+    }
+    else if (index < 0 || index > accs.length + 1)
+    {
+        puts(ERR_OOR);
+        goto prompt;
     }
     return index - 1;
 }
 
 void operate(int (*operation)(account *, double), int _index)
 {
-    while (true)
+restart:
+    double ammDeposit;
+    printf(AMMOUNT_PROMPT);
+    getDouble(&ammDeposit);
+
+    if (ammDeposit < 0)
     {
-        double ammDeposit;
-        printf(AMMOUNT_PROMPT);
-        if(cleanGetDouble(&ammDeposit)==-1) continue;;
-        if (ammDeposit <= 0)
+        goto restart;
+        puts(ERR_NEGATIVE_AMMOUT);
+    }
+    else
+    {
+        int rslt = operation(accs.elements + _index, ammDeposit);
+        switch (rslt)
         {
-            puts(ERR_WRONG_INPUT);
-        }
-        else
-        {
-            int rslt = operation(accs.elements + _index, ammDeposit);
-            switch (rslt)
-            {
-            case SUCCESS:
-                break;
-            case ERR_NO_IDENTITY:
-                break;
-            case ERR_NOT_ENOUGH_MONEY:
-                break;
-            default:
-                puts("unknown error  ");
-            }
-            writeData(accs);
-
+        case SUCCESS:
             puts(MSG_OPERATION_DONE);
+            writeData(accs);
             break;
+        case ERR_NO_IDENTITY:
+            break;
+        case ERR_NOT_ENOUGH_MONEY:
+            puts(NOT_ENOUGH_MONEY);
+            break;
+        default:
+            puts("unknown error  ");
         }
-
     }
 }
+
 void selectOperate(int (*operation)(account *, double))
 {
     int selected;
     selected = selectFromTable();
     system(CLS);
     printacc(accs.elements[selected]);
-    if (selected + 1)
+
+    switch (selected)
+    {
+    case BACK_TO_MENU:
+        return;
+        break;
+    default:
         operate(operation, selected);
+        break;
+    }
 }
+
 void printMenu()
 {
     printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
@@ -137,18 +152,64 @@ void printMenu()
            "7- show accounts list",
            "8- exit program");
 }
+
+void showTransferView()
+{
+    system(CLS);
+    puts("|Select Sender|");
+    int selectedSender = selectFromTable();
+    if (!(selectedSender + 1))
+        return;
+
+step_2:
+    system(CLS);
+    printf("Sender >>");
+    printaccTable(accs.elements[selectedSender]);
+    puts("|Select Receiver|");
+    int selectedReceiver = selectFromTable();
+    if (!(selectedReceiver + 1))
+        return;
+    if (selectedReceiver == selectedSender)
+        goto step_2;
+    system(CLS);
+    printf("Sender >>");
+    printaccTable(accs.elements[selectedSender]);
+
+    printf("Receiver >>");
+    printaccTable(accs.elements[selectedReceiver]);
+    printf(AMMOUNT_PROMPT);
+
+    double ammount;
+
+    getDouble(&ammount);
+
+    printf("enter a valid number>> ");
+
+    switch (transfer(accs.elements + selectedSender, accs.elements + selectedReceiver, ammount))
+    {
+    case SUCCESS:
+        puts(MSG_OPERATION_DONE);
+        writeData(accs);
+        break;
+    case ERR_NO_IDENTITY:
+        break;
+    case ERR_NOT_ENOUGH_MONEY:
+        break;
+    default:
+        break;
+    }
+}
+
 void Menu()
 {
     printMenu();
     int input;
-    if (cleanGetInt(&input) == -1)
-        return;
-    // printf("TODO %d",input);
+    printf(">> ");
+    getInt(&input);
     switch (input)
     {
     case MENU_CHOICE_1:
         system(CLS);
-        // getchar();// clears an extra newline from stdin character to avoid using it
         account newacc = createAccount();
         addElement(&accs, newacc);
         writeData(accs);
@@ -163,49 +224,7 @@ void Menu()
         selectOperate(withdraw);
         break;
     case MENU_CHOICE_4:
-        system(CLS);
-        puts("|Select Sender|");
-        int selectedSender = selectFromTable();
-        if (!(selectedSender + 1))
-            return ;
-
-        step_2:
-        system(CLS);
-        printf("Sender >>");
-        printaccTable(accs.elements[selectedSender]);
-        puts("|Select Receiver|");
-        int selectedReceiver = selectFromTable();
-        if (!(selectedReceiver + 1))
-            return ;
-        if(selectedReceiver == selectedSender)
-            goto step_2;
-        system(CLS);
-        printf("Sender >>");
-        printaccTable(accs.elements[selectedSender]);
-
-        printf("Receiver >>");
-        printaccTable(accs.elements[selectedReceiver]);
-        printf(AMMOUNT_PROMPT);
-        
-        double ammount;
-        
-        while (cleanGetDouble(&ammount) == -1 || ammount < 0)
-        {
-            printf("enter a valid number>> ");
-        }
-        switch(transfer(accs.elements + selectedSender, accs.elements + selectedReceiver, ammount)){
-            case SUCCESS:
-                puts(MSG_OPERATION_DONE);
-                writeData(accs);
-                break;
-            case ERR_NO_IDENTITY:
-                break;
-            case ERR_NOT_ENOUGH_MONEY:
-                break;
-            default:
-                break;
-        }
-        break;
+        showTransferView();
     case MENU_CHOICE_5:
 
         break;
